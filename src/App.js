dc4ba0a3d2b77841e568969b2b90dca912664f25
c00kamunga <tsothalanti2@yahoo.com>
2020-03-05 15:50:02 -0800

    imported useEffect from react, inserted axios call into useEffect, added dependency array

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from 'axios'; 
-
+import axios from "axios";
 
 axios
-.get('https://api.nasa.gov/#apod')
-.then(res => {
-  console.log(res)
-})
-.catch(err => {
-  console.log(err)
-})
-
+  .get("https://api.nasa.gov/#apod")
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 function App() {
   const [data, setData] = useState();
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/#apod")
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="App">
       <p>
